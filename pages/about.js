fd3b0855b373d73329ac98d53e1d58e26211cceb
jergra <jergra43@gmail.com>
2021-03-22 13:16:25 -0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+        <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, 
+                commodi veritatis consequuntur rem provident tempora, mollitia doloremque in, 
+                ullam deleniti perspiciatis a quas atque molestias asperiores explicabo 
+                voluptates? Voluptates, officiis?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, 
+                commodi veritatis consequuntur rem provident tempora, mollitia doloremque in, 
+                ullam deleniti perspiciatis a quas atque molestias asperiores explicabo 
+                voluptates? Voluptates, officiis?</p>
+        </div>
+        </>
+    );
+}
+ 
+export default About
